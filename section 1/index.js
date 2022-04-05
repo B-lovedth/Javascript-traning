b2db984995de680d3553447c5dba44d99ab613cd
Great Solomon <greatsoma2019@gmail.com>
2022-04-05 11:38:29 +0100

    Updated Index.js

--- section 1/index.js
+++ section 1/index.js
@@ -1,6 +1,6 @@
 let count = 0 // initializes count
-let saveEl = document.getElementById("save-el") // assigns the object from HTMl doc to the variable
-let countEl = document.getElementById("counting") // does the same thing from line 2
+const saveEl = document.getElementById("save-el") // assigns the object from HTMl doc to the variable
+const countEl = document.getElementById("counting") // does the same thing from line 2
 const increment=()=>{
     count++ // increments count by 1
     countEl.innerText = count // assigns the value of count to the text in HTML document
